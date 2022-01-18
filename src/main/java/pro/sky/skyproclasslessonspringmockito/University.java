d5745f1cd5ea5f437f790504328b3b546ef12b1a
Сергей <040620@mail.ru>
2022-01-18 12:12:14 +0300

    lecture-Spring-Mockito-lesson2.13

---
+++ src/main/java/pro/sky/skyproclasslessonspringmockito/University.java
@@ -0,0 +1,43 @@
+package pro.sky.skyproclasslessonspringmockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class University {
+    private final StudentValueGenerator studentValueGenerator;
+    private Map<Integer, Student> allStudents = new HashMap<>();
+    private int countId = 1;
+
+    public University(StudentValueGenerator studentValueGenerator) {
+        this.studentValueGenerator = studentValueGenerator;
+    }
+
+    public void addStudent(Student student) {
+        if (allStudents == null) {
+            allStudents = new HashMap<>();
+        }
+        student.setId(countId);
+        studentValueGenerator.generateAge();
+        student.setAge(studentValueGenerator.generateAge());
+        allStudents.put(countId, student);
+        countId++;
+    }
+
+    public void addStudentInRange(Student student, int minYear, int maxYear) {
+        if (allStudents == null) {
+            allStudents =new HashMap<Integer, Student>();
+        }
+        student.setId(countId);
+        studentValueGenerator.generateAge();
+        studentValueGenerator.generateAge();
+        student.setAge(studentValueGenerator.generateAgeInRange(minYear, maxYear));
+        allStudents.put(countId, student);
+        countId++;
+    }
+
+    public List<Student> getAllStudents() {
+        return new ArrayList<Student>(allStudents.values());
+    }
+}
